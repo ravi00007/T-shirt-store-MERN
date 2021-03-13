15cca71a0ac80bb61ba1ebed2688cf4c3ac5d9ab
Ravi Kumar <ravichouhan00007@gmail.com>
2021-03-13 12:57:27 +0530

    fix/Router is not a function

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,4 @@
-const express =  require('express');
-const router =  express().Router();
+const router = require("express").Router();
 const {} = require('../controllers/auth');
 const {} = require('../controllers/user');
 
